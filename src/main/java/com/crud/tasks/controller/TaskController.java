d0bbd6844d91f6ecd4bf4216be421c2e3779da8a
darek1975 <dchomontowski@wp.pl>
2019-07-16 09:16:00 +0200

    Spring WEB - REST API v4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -2,11 +2,10 @@ package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
-import com.crud.tasks.service.Dbservice;
+import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -16,7 +15,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @CrossOrigin(origins = "*")
 public class TaskController {
     @Autowired
-    private Dbservice service;
+    private DbService service;
 
     @Autowired
     private TaskMapper taskMapper;
